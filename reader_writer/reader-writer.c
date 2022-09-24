c16ca22795b41eb1048b0fee7776f2a3cc39dff9
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-09-25 01:57:16 +0530

    fix: modified with only mutexes

--- reader_writer/reader-writer.c
+++ reader_writer/reader-writer.c
@@ -9,8 +9,9 @@ struct read_write_lock
 {
 	// If required, use this strucure to create
 	// reader-writer lock related variables
-	pthread_cond_t readLock;
-	pthread_cond_t writeLock;
+	pthread_mutex_t readLock;
+	pthread_mutex_t writeLock;
+	pthread_mutex_t readTry;
 	pthread_mutex_t lock;
 	int readCount;
 	int writeCount;
@@ -30,9 +31,10 @@ long int data = 0; //	Shared data variable
 void InitalizeReadWriteLock(struct read_write_lock *rw)
 {
 	//	Initialize your reader-writer lock variables
-	pthread_cond_init(&rw->readLock, NULL);
-	pthread_cond_init(&rw->writeLock, NULL);
+	pthread_mutex_init(&rw->readLock, NULL);
+	pthread_mutex_init(&rw->writeLock, NULL);
 	pthread_mutex_init(&rw->lock, NULL);
+	pthread_mutex_init(&rw->readTry, NULL);
 	rw->readCount = 0;
 	rw->writeCount = 0;
 	rw->waitingReaders = 0;
@@ -42,70 +44,96 @@ void InitalizeReadWriteLock(struct read_write_lock *rw)
 void DestroyReadWriteLock(struct read_write_lock *rw)
 {
 	//	Destroy your reader-writer lock variables
-	pthread_cond_destroy(&rw->readLock);
-	pthread_cond_destroy(&rw->writeLock);
+	pthread_mutex_destroy(&rw->readLock);
+	pthread_mutex_destroy(&rw->writeLock);
 	pthread_mutex_destroy(&rw->lock);
+	pthread_mutex_destroy(&rw->readTry);
 }
 
 // The pthread based reader lock
 void ReaderLock(struct read_write_lock *rw)
 {
-	//	Acquire a reader lock
-	pthread_mutex_lock(&rw->lock);
-
-	if (rw->writeCount == 1 || rw->waitingWriters > 0)
-	{
-		rw->waitingReaders++;
-		pthread_cond_wait(&rw->readLock, &rw->lock);
-		rw->waitingReaders--;
-	}
-
+	//	Implement reader lock
+	pthread_mutex_lock(&rw->readTry);
+	pthread_mutex_lock(&rw->readLock);
 	rw->readCount++;
+	if (rw->readCount == 1)
+	{
+		pthread_mutex_lock(&rw->lock);
+	}
+	pthread_mutex_unlock(&rw->readLock);
+	pthread_mutex_unlock(&rw->readTry);
+
+	// if (rw->writeCount == 1 || rw->waitingWriters > 0)
+	// {
+	// 	rw->waitingReaders++;
+	// 	pthread_cond_wait(&rw->readLock, &rw->lock);
+	// 	rw->waitingReaders--;
+	// }
 }
 
 // The pthread based reader unlock
 void ReaderUnlock(struct read_write_lock *rw)
 {
-	pthread_mutex_unlock(&rw->lock);
+	//	Implement reader unlock
+	pthread_mutex_lock(&rw->readLock);
 	rw->readCount--;
-	//	Release a reader lock
-	if (rw->waitingWriters > 0)
+	if (rw->readCount == 0)
 	{
-		pthread_cond_signal(&rw->writeLock);
+		pthread_mutex_unlock(&rw->lock);
 	}
-	else
-	{
-		pthread_cond_broadcast(&rw->readLock);
-	}
+	pthread_mutex_unlock(&rw->readLock);
+	// if (rw->waitingWriters > 0)
+	// {
+	// 	pthread_cond_signal(&rw->writeLock);
+	// }
+	// else
+	// {
+	// 	pthread_cond_broadcast(&rw->readLock);
+	// }
 }
 
 // The pthread based writer lock
 void WriterLock(struct read_write_lock *rw)
 {
-	pthread_mutex_lock(&rw->lock);
-	if (rw->readCount > 0 || rw->writeCount == 1)
+	pthread_mutex_lock(&rw->writeLock);
+	rw->writeCount++;
+	if (rw->writeCount == 1)
 	{
-		rw->waitingWriters++;
-		pthread_cond_wait(&rw->writeLock, &rw->lock);
-		rw->waitingWriters--;
+		pthread_mutex_lock(&rw->readTry);
 	}
+	pthread_mutex_unlock(&rw->writeLock);
+	pthread_mutex_lock(&rw->lock);
+	// if (rw->readCount > 0 || rw->writeCount == 1)
+	// {
+	// 	rw->waitingWriters++;
+	// 	pthread_cond_wait(&rw->writeLock, &rw->lock);
+	// 	rw->waitingWriters--;
+	// }
 
-	rw->writeCount = 1;
+	// rw->writeCount = 1;
 }
 
 // The pthread based writer unlock
 void WriterUnlock(struct read_write_lock *rw)
 {
 	pthread_mutex_unlock(&rw->lock);
-	rw->writeCount = 0;
-	if (rw->waitingWriters > 0)
+	pthread_mutex_lock(&rw->writeLock);
+	rw->writeCount--;
+	if (rw->writeCount == 0)
 	{
-		pthread_cond_signal(&rw->writeLock);
+		pthread_mutex_unlock(&rw->readTry);
 	}
-	else
-	{
-		pthread_cond_broadcast(&rw->readLock);
-	}
+	pthread_mutex_unlock(&rw->writeLock);
+	// rw->writeCount = 0;
+	// if (rw->waitingWriters > 0)
+	// {
+	// 	pthread_cond_signal(&rw->writeLock);
+	// }
+	// else
+	// {
+	// 	pthread_cond_broadcast(&rw->readLock);
+	// }
 }
 
 //	Call this function to delay the execution by 'delay' ms
