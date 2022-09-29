6a3b9adf3231e90548e1bfc10383f09dc42a598a
Debojeet Das <debojeetofficial@gmail.com>
2022-09-29 23:02:10 +0530

    fix: added latest changes

--- million/addmillion.c
+++ million/addmillion.c
@@ -2,41 +2,52 @@
 #include <stdio.h>
 #include <pthread.h>
 
-int account_balance = 0;
+long int account_balance = 0;
 
 pthread_mutex_t lock;
 
-void *increment()
+void *increment(void *args)
 {
-    for (int i = 0; i < 1000000; i++)
+    int noMillion = *(int *)args;
+    for (int i = 0; i < noMillion; i++)
     {
-        pthread_mutex_lock(&lock);
-        account_balance++;
-        pthread_mutex_unlock(&lock);
+        for (int i = 0; i < 1000000; i++)
+        {
+            pthread_mutex_lock(&lock);
+            account_balance++;
+            pthread_mutex_unlock(&lock);
+        }
     }
 }
 
 int main(int argc, char *argv[])
 {
-    int threadNum = 10;
+    if (argc != 2)
+    {
+        printf("Usage: %s <number of threads>\n", argv[0]);
+        exit(1);
+    }
+
+    int threadNum = atoi(argv[1]);
     pthread_t th[threadNum];
+
+    int noMillion = 512 / threadNum;
 
     if (pthread_mutex_init(&lock, NULL) != 0)
     {
         printf("Mutex init failed\n");
     }
 
-    int i;
-    for (i = 0; i < threadNum; i++)
+    for (int i = 0; i < threadNum; i++)
     {
-        if (pthread_create(th + i, NULL, &increment, NULL) != 0)
+        if (pthread_create(th + i, NULL, &increment, (void *)&noMillion) != 0)
         {
             perror("Failed to create thread");
             return 1;
         }
         printf("Transaction %d has started\n", i);
     }
-    for (i = 0; i < threadNum; i++)
+    for (int i = 0; i < threadNum; i++)
     {
         if (pthread_join(th[i], NULL) != 0)
         {
@@ -44,7 +55,7 @@ int main(int argc, char *argv[])
         }
         printf("Transaction %d has finished\n", i);
     }
-    printf("Account Balance is : %d\n", account_balance);
+    printf("Account Balance is : %ld\n", account_balance);
 
     if (pthread_mutex_destroy(&lock) != 0)
     {
